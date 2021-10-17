14693a4f04a392f58edd9ad14dc6c4b2d011f672
Maxim Belkin <belkinurban@gmail.com>
2021-10-17 15:24:51 +0200

    Add the product id to the path

--- models/product.js
+++ models/product.js
@@ -26,6 +26,8 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = new Date().getTime().toString();
+
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
